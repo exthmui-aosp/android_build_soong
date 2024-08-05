4b7469271e39b189f4a44a211e067ea7a1ea1c26
Inseob Kim <inseob@google.com>
2024-08-05 12:51:05 +0900

    Add system_ext support to build_prop module
    As some partitions have build.prop under etc/, this change adds
    relative_install_path property to build_prop module. Also this change
    adds system_ext related Soong variables and system_ext support in
    gen_build_prop.py.
    
    Bug: 322090587
    Test: build and compare system_ext/etc/build.prop
    Change-Id: I416662b8bae09383af0cdd3d8444a5c300006b7b

--- android/variable.go
+++ android/variable.go
@@ -536,7 +536,8 @@ type ProductVariables struct {
 
 	ArtTargetIncludeDebugBuild *bool `json:",omitempty"`
 
-	SystemPropFiles []string `json:",omitempty"`
+	SystemPropFiles    []string `json:",omitempty"`
+	SystemExtPropFiles []string `json:",omitempty"`
 
 	EnableUffdGc *string `json:",omitempty"`
 }
